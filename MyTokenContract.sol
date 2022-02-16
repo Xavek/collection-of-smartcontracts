8c5662a9cb8b6397d5d7826c59e3d3a20091b631
gautam2002 <theofficalgautam200@gmail.com>
2022-02-16 11:16:29 +0545

    init commit

---
+++ MyTokenContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.2 <0.9.0;
+
+/* 
+User Would just send the hello and 1 ether to the contract and then we would send them the 10 trussCoin.
+@Name of coin: TrussCoin
+@Symbol: TC
+@TotalSupply: 10M
+@Decimals: standard ERC20 -18
+@mint: 1M to deployer or owner of the contract
+Currently looking to the Openzepline Standards
+*/
---
+++ readme.md
@@ -0,0 +1,5 @@
+## List of Smart Contracts
+
+**_Solidity Version: ^0.8.0_**
+
+- MyTokenContract : OpenZeppelin Standard ERC 20 Token.
