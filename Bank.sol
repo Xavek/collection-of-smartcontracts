bae31df8dffa7b409a729ad14ec23e9d950dd75d
gautam2002 <theofficalgautam200@gmail.com>
2022-03-31 18:31:53 +0545

    added compound interfaces

--- Bank.sol
+++ Bank.sol
@@ -6,16 +6,43 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title Bank
  * @dev User send the Eth to the Contract and the contract would stake it to the Compound and would get the return
    with comission in between.
+   Write the function modifiers that allows only owner to call the underlying method of transfering the eth to ceth
+   Now have to see how to mint user added eth to 
+
    
  */
+interface Erc20 {
+    function approve(address, uint256) external returns (bool);
+
+    function transfer(address, uint256) external returns (bool);
+}
+
+interface CErc20 {
+    function mint(uint256) external returns (uint256);
+
+    function exchangeRateCurrent() external returns (uint256);
+
+    function supplyRatePerBlock() external returns (uint256);
+
+    function redeem(uint256) external returns (uint256);
+
+    function redeemUnderlying(uint256) external returns (uint256);
+}
+
+interface CEth {
+    function mint() external payable;
+
+    function exchangeRateCurrent() external returns (uint256);
+
+    function supplyRatePerBlock() external returns (uint256);
+
+    function redeem(uint256) external returns (uint256);
+
+    function redeemUnderlying(uint256) external returns (uint256);
+}
+
 contract Bank {
-    // struct UserInfo = {
-    //     address userAddr;
-    //     uint userFund;
-    // }
-    // 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
-    // []UserInfo private aboutUser
-    address private owner;
+    address payable owner;
     uint256 public total_collection;
     event Sucess(address from, uint256 amount);
     event Sent(address from, address to, uint256 amnt);
@@ -23,22 +50,57 @@ contract Bank {
     error InsufficientBalance(uint256 requested, uint256 avilable);
 
     constructor() {
-        owner = msg.sender;
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only owner is allowed to call this function"
+        );
+        _;
     }
 
     mapping(address => uint256) private userAmount;
+    event MyLog(string, uint256);
+
+    function supplyErc20ToCompound(
+        // address _erc20Contract,
+        address _cErc20Contract,
+        uint256 _numTokensToSupply
+    ) public onlyOwner returns (uint256) {
+        // Createating  reference to the underlying asset contract, like DAI.
+        // Erc20 underlying = Erc20(_erc20Contract);
+
+        // Create a reference to the corresponding cToken contract, like cDAI
+        CErc20 cToken = CErc20(_cErc20Contract);
+
+        // Amount of current exchange rate from cToken to underlying
+        uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
+        emit MyLog("Exchange Rate (scaled up): ", exchangeRateMantissa);
+
+        // Amount added to you supply balance this block
+        uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
+        emit MyLog("Supply Rate: (scaled up)", supplyRateMantissa);
+
+        // Approve transfer on the ERC20 contract
+        // underlying.approve(_cErc20Contract, _numTokensToSupply);
+
+        // Mint cTokens
+        uint256 mintResult = cToken.mint(_numTokensToSupply);
+        return mintResult;
+    }
 
     // first open the bank account that would be store their address
     function openAcc(address _address, uint256 _ammount) external {
         require(_address != owner, "Bank Owner cannot Call");
-        require(_ammount >= 1, "Minimum 1 eth required");
+        require(_ammount >= 1 ether, "Minimum 1 eth required");
         userAmount[_address] = _ammount;
         total_collection += _ammount;
         emit Sucess(_address, _ammount);
     }
 
     // to check which address have the how much value
-
     function getBalance(address _address) public view returns (uint256) {
         return userAmount[_address];
     }
@@ -57,6 +119,4 @@ contract Bank {
         userAmount[_to] += _ammount;
         emit Sent(_from, _to, _ammount);
     }
-
-    // 0xdD870fA1b7C4700F2BD7f44238821C26f7392148
 }
