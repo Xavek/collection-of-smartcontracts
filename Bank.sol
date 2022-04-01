6ec5b4ee3f4fcbde19de69b9e30c53296918caac
gautam2002 <theofficalgautam200@gmail.com>
2022-04-01 11:16:27 +0545

    added ceth rinkeby testnet

--- Bank.sol
+++ Bank.sol
@@ -6,28 +6,10 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title Bank
  * @dev User send the Eth to the Contract and the contract would stake it to the Compound and would get the return
    with comission in between.
-   Write the function modifiers that allows only owner to call the underlying method of transfering the eth to ceth
-   Now have to see how to mint user added eth to 
+       
 
    
  */
-interface Erc20 {
-    function approve(address, uint256) external returns (bool);
-
-    function transfer(address, uint256) external returns (bool);
-}
-
-interface CErc20 {
-    function mint(uint256) external returns (uint256);
-
-    function exchangeRateCurrent() external returns (uint256);
-
-    function supplyRatePerBlock() external returns (uint256);
-
-    function redeem(uint256) external returns (uint256);
-
-    function redeemUnderlying(uint256) external returns (uint256);
-}
 
 interface CEth {
     function mint() external payable;
@@ -45,7 +27,7 @@ contract Bank {
     address payable owner;
     uint256 public total_collection;
     event Sucess(address from, uint256 amount);
-    event Sent(address from, address to, uint256 amnt);
+    // event Sent(address from, address to, uint amnt);
 
     error InsufficientBalance(uint256 requested, uint256 avilable);
 
@@ -64,59 +46,58 @@ contract Bank {
     mapping(address => uint256) private userAmount;
     event MyLog(string, uint256);
 
-    function supplyErc20ToCompound(
-        // address _erc20Contract,
-        address _cErc20Contract,
-        uint256 _numTokensToSupply
-    ) public onlyOwner returns (uint256) {
-        // Createating  reference to the underlying asset contract, like DAI.
-        // Erc20 underlying = Erc20(_erc20Contract);
-
-        // Create a reference to the corresponding cToken contract, like cDAI
-        CErc20 cToken = CErc20(_cErc20Contract);
+    function supplyEthToCompound(address payable _cEtherContract)
+        public
+        payable
+        returns (bool)
+    {
+        // Create a reference to the corresponding cToken contract
+        CEth cToken = CEth(_cEtherContract);
 
         // Amount of current exchange rate from cToken to underlying
         uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
-        emit MyLog("Exchange Rate (scaled up): ", exchangeRateMantissa);
+        emit MyLog("Exchange Rate (scaled up by 1e18): ", exchangeRateMantissa);
 
         // Amount added to you supply balance this block
         uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
-        emit MyLog("Supply Rate: (scaled up)", supplyRateMantissa);
+        emit MyLog("Supply Rate: (scaled up by 1e18)", supplyRateMantissa);
 
-        // Approve transfer on the ERC20 contract
-        // underlying.approve(_cErc20Contract, _numTokensToSupply);
-
-        // Mint cTokens
-        uint256 mintResult = cToken.mint(_numTokensToSupply);
-        return mintResult;
+        cToken.mint{value: 0.008 ether, gas: 250000}();
+        return true;
     }
 
-    // first open the bank account that would be store their address
-    function openAcc(address _address, uint256 _ammount) external {
-        require(_address != owner, "Bank Owner cannot Call");
-        require(_ammount >= 1 ether, "Minimum 1 eth required");
-        userAmount[_address] = _ammount;
-        total_collection += _ammount;
-        emit Sucess(_address, _ammount);
+    function redeemCEth(
+        uint256 amount,
+        bool redeemType,
+        address _cEtherContract
+    ) public returns (bool) {
+        // Create a reference to the corresponding cToken contract
+        CEth cToken = CEth(_cEtherContract);
+
+        // `amount` is scaled up by 1e18 to avoid decimals
+
+        uint256 redeemResult;
+
+        if (redeemType == true) {
+            // Retrieve your asset based on a cToken amount
+            redeemResult = cToken.redeem(amount);
+        } else {
+            // Retrieve your asset based on an amount of the asset
+            redeemResult = cToken.redeemUnderlying(amount);
+        }
+
+        // Error codes are listed here:
+        // https://compound.finance/docs/ctokens#error-codes
+        emit MyLog("If this is not 0, there was an error", redeemResult);
+
+        return true;
     }
+
+    // This is needed to receive ETH when calling `redeemCEth`
+    receive() external payable {}
 
     // to check which address have the how much value
     function getBalance(address _address) public view returns (uint256) {
         return userAmount[_address];
-    }
-
-    function transfer(
-        address _from,
-        address _to,
-        uint256 _ammount
-    ) external {
-        if (_ammount > userAmount[_from])
-            revert InsufficientBalance({
-                requested: _ammount,
-                avilable: userAmount[_from]
-            });
-        userAmount[_from] -= _ammount;
-        userAmount[_to] += _ammount;
-        emit Sent(_from, _to, _ammount);
     }
 }
--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@
 
 - MyTokenContract : IERC20 Interfaces. It consist of both six mandatory set of functions and three optional ones with two events.
 
-- Bank Contract : Contract would take ethers from the User and under the hood would stake upon Compound Protocol(Rinkeby Test) and provide the Interest(given by compound). Contract would take a %cut from the Interest.
+- Bank Contract : Contract would take ethers from the User and under the hood would stake upon Compound Protocol(cEth Rinkebt Test Network) and provide the Interest(given by compound cEth). Contract would take a %cut from the Interest when user withdraws
